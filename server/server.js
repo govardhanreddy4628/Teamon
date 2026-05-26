40400ac2b227ca8ea127cd7630b8d0980bbf799f
Your name <you@example.com>
2026-05-26 20:27:19 +0530

    update: project and task improvements

--- server/server.js
+++ server/server.js
@@ -14,12 +14,16 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5174"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173", "http://127.0.0.1:5174"],
+//     credentials: true,
+//   })
+// );
+
+app.use(cors({
+  origin: "*",
+}));
 
 app.use(clerkMiddleware());
 
